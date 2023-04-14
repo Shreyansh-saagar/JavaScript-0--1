506e1f1b02c4fac7f9d6f61d4fb05074a0e39806
Shreyansh saagar <Shreyanshsaagar@gmail.com>
2023-04-15 00:31:34 +0530

    Add files via upload

---
+++ index_4.js
@@ -0,0 +1,7 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.caption)
+console.log(t.tHead.firstElementChild)
+console.log(t.Foot)
+console.log(t.tBodies)
+console.log(t.rows[0].rowIndex)
